f99be507293ead23b823cf57adc527f02149ef6c
rikisorsa <33903170+rikisorsa@users.noreply.github.com>
2025-01-29 11:06:51 +0200

    connection changes02

--- server.js
+++ server.js
@@ -30,4 +30,5 @@ app.use('/api/products', productRoutes);
 
 // Start the server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on http://94.237.32.45:${PORT}`));
+app.listen(PORT, '0.0.0.0', () => console.log(`Server running on http://94.237.32.45:${PORT}`));
+
